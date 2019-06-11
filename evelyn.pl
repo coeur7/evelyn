e2ad55aed5e45571eb4d8b41e3aa04e3c88c2881
coeur7 <47952676+coeur7@users.noreply.github.com>
2019-06-11 06:20:29 +0200

    EVELYN v1.4: Rating system added; endless loop on roster/method mismatch no longer occurs.
    See README for details on how to rate.

--- evelyn.pl
+++ evelyn.pl
@@ -5,12 +5,16 @@ BEGIN {
     use warnings;
     use POSIX;
     
+    our $EVELYN_version = "1.4";
+    
     our $method;
     our $verbosity = 1;                     # 0 is silent; other levels undefined rn. TODO: configuration file
     our $roster_filename  = "./roster.txt"; # roster file containing information pertaining directly to roll/method
-    our $output_filename  = "./roll.txt";   # contains the roll after execution
+    our $roll_filename    = "./roll.txt";   # contains the roll after execution
     our $runlog_filename  = "./runlog.txt"; # contains all the rolls of the run TODO
+    our $ratings_filename = "./ratings.txt";# contains all the ratings
     our $details_filename = "";             # set details, read from roster file TODO
+    our $max_roll_attempts = 20000;
     
     srand();
 }
@@ -57,9 +61,7 @@ sub roll {
         my @candidates = @{$group->[2]};
         my @roll;
         my $roll_is_valid = 0;
-        
-        printf $file "%-32s", $category.":";
-        if ($verbosity) { print $category.": "; }
+        my $i = 0;
         
         do {
             @roll = ();
@@ -79,17 +81,97 @@ sub roll {
             if($method eq "asterisk" || $method eq "asterisk-nodupl") {
                 if (grep(/\*/, @roll)) { $roll_is_valid = 1; }
             } else { $roll_is_valid = 1; } # always
+            
+            $i++;
+            if ($i > $max_roll_attempts) { die "Tried ".$max_roll_attempts." rolls; none were valid by the method in use.\n"; }
         } while (!($roll_is_valid));
         
+        printf $file "%-32s", $category.":";
+        if ($verbosity) { print $category.": "; }
         foreach my $r (@roll) { print $file "$r, "; if ($verbosity) { print "$r, "; } } 
         print $file "\n"; if ($verbosity) { print "\n"; }
     }
     close($file);
+    
+    return 0;
+}
+
+sub rate {
+    my $filename    = shift @_;
+    my $new_key     = shift @_;
+    my $new_val     = shift @_;
+    my $mode        = shift @_;
+    
+    my %ratings;
+    my %ns;
+    
+    open(my $file, "<", $filename) || die "[FATAL] Could not create or open ratings file '".$filename."':".$!;
+    while (my $a = <$file>) {
+        $a =~ m/^(.+?)\s+(.+)\s+([0-9]+)\s*\n$/g;
+        if(defined $1 && defined $2 && defined $3) {
+            $ratings{$1} = $2;
+            $ns{$1}      = $3;
+        }
+    }
+    close($file);
+    
+    if($mode == 1 && $ns{$new_key} != 0) {
+        $ratings{$new_key} = ($ratings{$new_key} + $new_val / $ns{$new_key}) * $ns{$new_key}/($ns{$new_key}+1); $ns{$new_key}++;
+        print "DEBUG MODE 1: ".$ratings{$new_key}."\n";
+    } else { $ratings{$new_key} = $new_val; $ns{$new_key}++; print "DEBUG MODE 0: ".$ratings{$new_key}."\n";}
+    
+    open(my $file, ">", $filename) || die "[FATAL] Could not create or open ratings file '".$filename."':".$!;
+    foreach my $k (keys %ratings) {
+        print $file $k." ".$ratings{$k}." ".$ns{$k}."\n";
+    }
+    close($file);
+    
+    return 0;
 }
 
 sub main {
-    my @roster = read_roster("./roster.txt");
-    roll("./roll.txt", @roster);
+    # Help mode
+    if($ARGV[0] =~ m/^-[A-Z|a-z|?]*\?/g) {
+        print "-?                  Usage help\n";
+        print "-g                  Generate ratings file for roster\n";
+        print "-r  SPECIES RATING  Rate SPECIES with RATING (added and averaged)\n";
+        print "-r0 SPECIES RATING  Rate SPECIES with RATING (reset rating to new value)\n";
+        print "-v                  Print current version\n";
+        return 0;
+    }
+    
+    # Version mode
+    if($ARGV[0] =~ m/^-[A-Z|a-z|?]*v/g) {
+        print "EVELYN version ".$EVELYN_version."\n";
+        return 0;
+    }
+    
+    # Generate ratings file for roster
+    if($ARGV[0] =~ m/^-g$/g) {
+        my @roster = read_roster($roster_filename);
+        open(my $file, ">", $ratings_filename) || die "[FATAL] Could not create or open ratings file '".$filename."':".$!;
+        foreach my $group (@roster) {
+            foreach my $k (@{$group->[2]}) {
+                print $file $k." 0 0\n";
+            }
+        }
+        close($file);
+        return 0;
+    }
+    
+    # Rating mode
+    if($ARGV[0] =~ m/^-r0$/g) {
+        rate($ratings_filename, $ARGV[1], $ARGV[2], 0);
+        return 0;
+    }
+    if($ARGV[0] =~ m/^-r$/g) {
+        rate($ratings_filename, $ARGV[1], $ARGV[2], 1);
+        return 0;
+    }
+
+    # Roll mode
+    my @roster = read_roster($roster_filename);
+    roll($roll_filename, @roster);
     
     return 0;
 }
