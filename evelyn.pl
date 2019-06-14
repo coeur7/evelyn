b3ceebe69dacb5d850d06b275911c2431728acc9
coeur7 <47952676+coeur7@users.noreply.github.com>
2019-06-14 08:18:08 +0200

    EVELYN v1.6: runlog; roll method rewritten

--- evelyn.pl
+++ evelyn.pl
@@ -4,17 +4,20 @@ BEGIN {
     use strict;
     use warnings;
     use POSIX;
+    use Scalar::Util qw(looks_like_number);
     
-    our $EVELYN_version = "1.4";
+    our $EVELYN_version = "1.6";
     
-    our $method;
+    our $method;                            # Supported atm: repto, coeur, asterisk, asterisk-nodupl
     our $verbosity = 1;                     # 0 is silent; other levels undefined rn. TODO: configuration file
+    
     our $roster_filename  = "./roster.txt"; # roster file containing information pertaining directly to roll/method
     our $roll_filename    = "./roll.txt";   # contains the roll after execution
     our $runlog_filename  = "./runlog.txt"; # contains all the rolls of the run TODO
     our $ratings_filename = "./ratings.txt";# contains all the ratings
-    our $details_filename = "";             # set details, read from roster file TODO
+    our $details_filename = "";             # set details, read from roster file
     our $paste_out_filename = "./team.txt"; # paste output filename
+    
     our $max_roll_attempts = 20000;
     
     srand();
@@ -50,12 +53,30 @@ sub read_roster {
 }
 
 sub roll {
-    my $filename = shift @_;
-    open(my $file, ">", $filename) || die "[FATAL] Could not create or open roll output file '".$filename."':".$!;
+    my $filename    = shift @_;
+    my @roster      = @_;
+    my $roll_number = 0;
     
-    my @roster = @_;
+    if(open(my $runlog_file, "<", $runlog_filename)) {
+        while(my $a = <$runlog_file>) {
+            if($a =~ m/^Roll #([0-9]+)\n$/g) { $roll_number = int($1) + 1; }
+        }
+        close($runlog_file);
+    } else {$roll_number = 1; }    
+    
+    open(my $roll_file, ">", $filename) || die "[FATAL] Could not create or open roll output file '".$filename."':".$!;
+    open(my $runlog_file, ">>", $runlog_filename) || die "[FATAL] Could not create or open run log file '".$runlog_filename."':".$!;
+    
+    my @print_targets = ($roll_file, $runlog_file);
+    if ($verbosity) { push(@print_targets, STDOUT); }
     
     print "[EVELYN] Rolling according to the '".$method."' method.\n";
+    
+    foreach my $target (@print_targets) {
+        print $target "-"x80, "\n";
+        print $target "Roll #".$roll_number."\n";
+    }
+    
     foreach $group (@roster) {
         my $category = $group->[0];
         my $n        = $group->[1];
@@ -85,15 +106,61 @@ sub roll {
             
             $i++;
             if ($i > $max_roll_attempts) { die "Tried ".$max_roll_attempts." rolls; none were valid by the method in use.\n"; }
-        } while (!($roll_is_valid));
+        } while (! $roll_is_valid);
         
-        printf $file "%-32s", $category.":";
-        if ($verbosity) { print $category.": "; }
-        foreach my $r (@roll) { print $file "$r, "; if ($verbosity) { print "$r, "; } } 
-        print $file "\n"; if ($verbosity) { print "\n"; }
+        foreach my $target (@print_targets) {
+            printf $target "%-32s", $category.":";
+            foreach my $r(@roll) { print $target "$r, "; }
+            print $target "\n";
+        }
+    }
+    
+    foreach my $target (@print_targets) {
+        print $target "-"x80, "\n";
+    }
+    
+    close($roll_file);
+    close($runlog_file);
+    
+    return 0;
+}
+
+sub export_team {
+    if($details_filename eq "") {
+        die "[FATAL] No roster details filename supplied (add the line \"details in ___\", replacing the underscores with your filename, to ".$roster_filename.").";
+    }
+    open(my $details_file, "<", $details_filename) || die "[FATAL] Could not create or open roster details file '".$details_filename."':".$!;
+    open(my $out_file, ">", $paste_out_filename) || die "[FATAL] Could not create or open paste output file '".$paste_out_filename."':".$!;
+    foreach my $set (@ARGV[1..$#ARGV]) {
+        my $mode = 0;
+        while(my $a = <$details_file>) {
+            if($a =~ m/^\s*(.+?)\s*[(|@].*\n$/g && $1 eq $set) {
+                $mode = 1;
+            }
+            if($mode) { print $out_file $a; }
+            if($a =~ m/^\s*\n$/g) {
+                $mode = 0;
+            }
+        }
+        seek ($details_file, 0, 0);
+    }
+    close($details_file);
+    close($out_file);
+}
+
+sub generate_ratings_file {
+    if (-f $ratings_filename) {
+        print "[WARNING] Ratings file '".$ratings_filename."' already exists; no action taken.\n(To create a new ratings file, please delete the existing one manually.) \n";
+        return 0;
+    }
+    my @roster = read_roster($roster_filename);
+    open(my $file, ">", $ratings_filename) || die "[FATAL] Could not create or open ratings file '".$filename."':".$!;
+    foreach my $group (@roster) {
+        foreach my $k (@{$group->[2]}) {
+            print $file $k." 0 0\n";
+        }
     }
     close($file);
-    
     return 0;
 }
 
@@ -103,13 +170,18 @@ sub rate {
     my $new_val     = shift @_;
     my $mode        = shift @_;
     
+    if(! looks_like_number($new_val)) {
+        print "[FATAL] '".$new_val."' does not appear to be a numeric value. Aborting.\n";
+        return -1;
+    }
+    
     my %ratings;
     my %ns;
     
     open(my $file, "<", $filename) || die "[FATAL] Could not create or open ratings file '".$filename."':".$!;
     while (my $a = <$file>) {
         $a =~ m/^(.+?)\s+(.+)\s+([0-9]+)\s*\n$/g;
-        if(defined $1 && defined $2 && defined $3) {
+        if(defined $1 && looks_like_number($2) && looks_like_number($3)) {
             $ratings{$1} = $2;
             $ns{$1}      = $3;
         }
@@ -150,38 +222,13 @@ sub main {
     # Export mode
     if($ARGV[0] =~ m/^-d$/g) {
         my @roster = read_roster($roster_filename);
-        open(my $file, "<", $details_filename) || die "[FATAL] Could not create or open roster details file '".$details_filename."':".$!;
-        open(my $out_file, ">", $paste_out_filename) || die "[FATAL] Could not create or open paste output file '".$paste_out_filename."':".$!;
-        foreach my $set (@ARGV[1..$#ARGV]) {
-            my $mode = 0;
-            while(my $a = <$file>) {
-                if($a =~ m/^\s*(.+?)\s*[(|@].*\n$/g && $1 eq $set) {
-                    $mode = 1;
-                }
-                if($mode) { print $out_file $a; }
-                if($a =~ m/^\s*\n$/g) {
-                    $mode = 0;
-                }
-            }
-            seek ($file, 0, 0);
-        }
+        export_team();
         return 0;
     }
     
     # Generate ratings file for roster
     if($ARGV[0] =~ m/^-g$/g) {
-        if (-f $ratings_filename) {
-            print "[WARNING] Ratings file '".$ratings_filename."' already exists; please manually delete it and retry.\n";
-            return 0;
-        }
-        my @roster = read_roster($roster_filename);
-        open(my $file, ">", $ratings_filename) || die "[FATAL] Could not create or open ratings file '".$filename."':".$!;
-        foreach my $group (@roster) {
-            foreach my $k (@{$group->[2]}) {
-                print $file $k." 0 0\n";
-            }
-        }
-        close($file);
+        generate_ratings_file();
         return 0;
     }
     
